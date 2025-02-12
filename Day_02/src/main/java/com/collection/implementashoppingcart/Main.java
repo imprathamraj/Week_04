d96640e89126f9ba8db7dcb0d7da3ff0814e3270
imprathamraj <rjpratham05@gmail.com>
2025-02-12 11:48:28 +0530

    [Pratham Raj] Add. Main

---
+++ Day_02/src/main/java/com/collection/implementashoppingcart/Main.java
@@ -0,0 +1,22 @@
+package org.collectionframwork.shoppingcart;
+
+import com.collection.implementashoppingcart.CartDisplay;
+import com.collection.implementashoppingcart.CartManager;
+
+public class Main {
+    public static void main(String[] args) {
+        CartManager manager = new CartManager();
+        manager.addProduct("Laptop", 800.00);
+        manager.addProduct("Phone", 500.00);
+        manager.addProduct("Headphones", 100.00);
+
+
+        manager.addToCart("Phone");
+        manager.addToCart("Laptop");
+        manager.addToCart("Headphones");
+        manager.addToCart("custom");
+        System.out.println("-------------------------");
+
+        CartDisplay.displaySortedCart(manager.getCart());
+    }
+}
