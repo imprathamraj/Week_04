cc0ea4c361c0443bb5054946b229e01617fe83fa
imprathamraj <rjpratham05@gmail.com>
2025-02-12 11:08:47 +0530

    [Pratham Raj] Add. Hospital Triage System

---
+++ Day_02/src/main/java/com/collection/queue/hospitaltriagesystem/Patient.java
@@ -0,0 +1,11 @@
+package com.collection.queue.hospitaltriagesystem;
+
+class Patient {
+    String name;
+    int severity;
+
+    public Patient(String name, int severity) {
+        this.name = name;
+        this.severity = severity;
+    }
+}
