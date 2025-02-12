a6886878b3c8f773c4679b9db6ff94d706cc3b29
imprathamraj <rjpratham05@gmail.com>
2025-02-12 09:56:55 +0530

    [Pratham Raj] Fix. InvertMapMain package

--- Day_02/src/main/java/com/collection/map/invertmap/InvertMapMain.java
+++ Day_02/src/main/java/com/collection/map/invertmap/InvertMapMain.java
@@ -3,7 +3,7 @@ Invert a Map<K, V> to produce a Map<V, K>. Handle duplicate values by storing th
 Example:
 Input: {A=1, B=2, C=1} → Output: {1=[A, C], 2=[B]}.
 */
-package com.collection.map.wordfrequencycounter.invertamap;
+package com.collection.map.invertmap;
 
 import java.util.*;
 
