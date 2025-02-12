3be1111078b56a741f03a4cf65587d423a50daea
imprathamraj <rjpratham05@gmail.com>
2025-02-12 11:50:19 +0530

    [Pratham Raj] Add. CartDisplay

--- Day_02/src/main/java/com/collection/implementashoppingcart/CartDisplay.java
+++ Day_02/src/main/java/com/collection/implementashoppingcart/CartDisplay.java
@@ -9,6 +9,7 @@ public class CartDisplay {
     public static void displaySortedCart(HashMap<String, Double> cart) {
         TreeMap<String, Double> sortedCart = new TreeMap<>(cart);
         System.out.println("Shopping Cart (Sorted by Product Name):");
+
         for (Map.Entry<String, Double> entry : sortedCart.entrySet()) {
             System.out.println(entry.getKey() + " : $" + entry.getValue());
         }
