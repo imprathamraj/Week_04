7ae78bc0cf45ea11c99838089ef8ea850d380488
imprathamraj <rjpratham05@gmail.com>
2025-02-12 10:08:29 +0530

    [Pratham Raj] Add. MaxValueKeyMain

---
+++ Day_02/src/main/java/com/collection/map/findthekeywiththehighestvalue/MaxValueKeyMain.java
@@ -0,0 +1,20 @@
+package com.collection.map.findthekeywiththehighestvalue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxValueKeyMain {
+    public static void main(String[] args) {
+        // Defining the input map
+        Map<String, Integer> inputMap = new HashMap<>();
+        inputMap.put("A", 10);
+        inputMap.put("B", 20);
+        inputMap.put("C", 15);
+
+        // Finding the key with the highest value
+        String maxKey = MaxValueKeyFinder.findMaxValueKey(inputMap);
+
+        // Printing the result
+        System.out.println("Key with highest value: " + maxKey);
+    }
+}
