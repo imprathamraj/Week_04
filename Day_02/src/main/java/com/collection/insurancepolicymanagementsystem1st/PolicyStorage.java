7757d5686e4b55f5ea0e7d7e5e0a31a72cca4b5e
imprathamraj <rjpratham05@gmail.com>
2025-02-12 12:24:20 +0530

    [Pratham Raj] Add. PolicyStorage

---
+++ Day_02/src/main/java/com/collection/insurancepolicymanagementsystem1st/PolicyStorage.java
@@ -0,0 +1,23 @@
+package com.collection.insurancepolicymanagementsystem1st;
+
+import java.util.*;
+
+public class PolicyStorage {
+    Set<Policy> hashSet = new HashSet<>();
+    Set<Policy> linkedHashSet = new LinkedHashSet<>();
+    Set<Policy> treeSet = new TreeSet<>();
+
+    public void addPolicy(Policy policy) {
+        hashSet.add(policy);
+        linkedHashSet.add(policy);
+        treeSet.add(policy);
+    }
+
+    public Set<Policy> getAllPolicies() {
+        return hashSet;
+    }
+
+    public Set<Policy> getSortedPolicies() {
+        return treeSet;
+    }
+}
