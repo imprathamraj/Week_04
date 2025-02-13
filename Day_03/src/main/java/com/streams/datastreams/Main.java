3ed4345aeb7d816d348495b09b5f865e73ddf89b
imprathamraj <rjpratham05@gmail.com>
2025-02-13 16:43:29 +0530

    [Pratham Raj] Add. Data Streams - Store and Retrieve Primitive Data

---
+++ Day_03/src/main/java/com/streams/datastreams/Main.java
@@ -0,0 +1,8 @@
+package com.streams.datastreams;
+
+public class Main {
+    public static void main(String[] args) {
+        DataStreams.storeStudentDetails();
+        DataStreams.retrieveStudentDetails();
+    }
+}
