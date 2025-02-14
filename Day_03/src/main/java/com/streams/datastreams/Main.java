7ecec2af7b372dd4cd24dabc356ba132b73c730f
Pratham Raj <130917920+imprathamraj@users.noreply.github.com>
2025-02-14 12:30:46 +0530

    Delete Day_03/src/main/java/com/streams directory

--- Day_03/src/main/java/com/streams/datastreams/Main.java
+++
@@ -1,8 +0,0 @@
-package com.streams.datastreams;
-
-public class Main {
-    public static void main(String[] args) {
-        DataStreams.storeStudentDetails();
-        DataStreams.retrieveStudentDetails();
-    }
-}
