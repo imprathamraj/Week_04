eb7a2b806ff1b12da6efb09d57d7a99beb149226
imprathamraj <rjpratham05@gmail.com>
2025-02-15 22:52:46 +0530

    [Pratham Raj] Add. Testing Date Formatte and Testing

---
+++ Day_05/src/main/java/com/junit/basic/testingbeforeeachandaftereachannotations/DatabaseConnection.java
@@ -0,0 +1,17 @@
+package com.junit.basic.testingbeforeeachandaftereachannotations;
+
+public class DatabaseConnection {
+    private boolean isConnected;
+
+    public void connect(){
+        isConnected= true;
+        System.out.println("Database is connected");
+    }
+    public void disconnect(){
+        isConnected= false;
+        System.out.println("Database is disconnected");
+    }
+    public boolean isConnected(){
+        return isConnected ;
+    }
+}
